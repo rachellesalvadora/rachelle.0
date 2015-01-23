c04bf64b27c636961f059062c7b5cccdcb830e77
Rachelle Salvadora <rachellesalvadora@gmail.com>
2015-01-23 14:40:31 +1100

    homepage finish

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+
+	<title>Rachelle|Salvadora</title>
+
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<div class="main-container">
+
+
---
+++ index.php
@@ -0,0 +1,32 @@
+
+<?php
+include "header.php";
+
+
+?>
+
+<div class="nav-main">
+			<div class="nav-div">
+				<ul class="navigation">
+					<li class="nav-li">
+						<a class="nav-a a-focus" href="#index.php">HOME</a>
+					</li>
+					<li class="nav-li">
+						<a class="nav-a" href="#about.php">ABOUT</a>
+					</li>
+					<li class="nav-li">
+						<a class="nav-a" href="#about.php">GALLERY</a>
+					</li>
+					<li class="nav-li">
+						<a class="nav-a" href="#contact.php">CONTACT</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+<h1 class="rachelle">
+	RACHELLE S.
+</h1>
+
+</div>
+</body>
