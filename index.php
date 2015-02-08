b7a0d3d8affc1f63e1e87a7e9f36954a8152a23e
Rachelle Salvadora <rachellesalvadora@gmail.com>
2015-02-08 12:45:32 +1100

    all pages scrollable and links working

--- index.php
+++ index.php
@@ -5,20 +5,27 @@ include "header.php";
 
 ?>
 
-<div class="nav-main">
+<div class="nav-main" id="menu">
 			<div class="nav-div">
 				<ul class="navigation">
-					<li class="nav-li">
-						<a class="nav-a a-focus" href="index.php">HOME</a>
+					<li class="nav-li" data-menuanchor="firstPage">
+						<a class="nav-a" href="#firstPage">HOME</a>
 					</li>
-					<li class="nav-li">
-						<a class="nav-a" href="about.php">ABOUT</a>
+					<li class="nav-li" data-menuanchor="secondPage">
+						<a class="nav-a" href="#secondPage">WHO</a>
 					</li>
-					<li class="nav-li">
-						<a class="nav-a" href="gallery.php">GALLERY</a>
+					<li class="nav-li" data-menuanchor="3rdPage">
+						<a class="nav-a" href="#3rdPage">CS50</a>
 					</li>
-					<li class="nav-li">
-						<a class="nav-a" href="contact.php">CONTACT</a>
+					<li class="nav-li" data-menuanchor="4thPage">
+						<a class="nav-a" href="#4thPage">TIMELINE</a>
+					</li>
+					<li class="nav-li" data-menuanchor="5thPage">
+						<a class="nav-a" href="#5thPage">PERSONAL</a>
+					</li>
+					
+					<li class="nav-li" data-menuanchor="lastPage">
+						<a class="nav-a" href="#lastPage">CONTACT</a>
 					</li>
 				</ul>
 			</div>
@@ -26,9 +33,125 @@ include "header.php";
 
 		</div>
 
-<h1 class="rachelle">
-	RACHELLE S.
-</h1>
+
+<div id="fullpage">
+	<div class="section " id="section0">
+		<div class="rachelle-div">
+
+			<h1 class="rachelle">
+				RACHELLE S.
+			</h1>
+		</div>
+	</div>
+	<div class="section" id="section1">
+		<div class="container">
+			<div class="inside-container">
+				<div class="about-left">
+					<h3 class="subtitle">Who's</h3> 
+					<h2 class="title">RACHELLE? </h2>
+				</div>
+
+				<div class="about-right" id="who">
+					<img class ="about-img" src="img/rachelle-whole.jpg" alt="about me image">
+					<p class="about-p">
+						I'm having a lot of fun playing around the <strong>tools and techniques</strong>, writing websites through different <strong>languages</strong> and pushing changes in <strong>git</strong>.
+					</p>
+					<p class="about-p">
+						I am using variety of <strong>applications and extensions</strong>, and applying <strong>frameworks</strong> and learning <strong>CMS</strong> that I discover around around the internet.
+					</p>
+					<p class="about-p">
+						Although I am more into front-end, I also try to be competent on the back-end.
+
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="section2">
+		<div class="container2"  id="cs50" >
+			<div class="inside-container" >
+				<div class="cs50-div" >
+					<p class="p-cs50 challenge">
+						<apostrophe>&#34;</apostrophe>Challenging but definitely doable.<apostrophe>&#34;</apostrophe></p>
+						</br></br>
+
+						<p class="certificate">
+							You can view my certified certificate here:
+						<br/><br/>
+							<img  class="cs50-img" src="img/certificate.png" alt="CS50 Certificate" />
+						<br/><br/>
+							<a class="cs50-link" href="https://os.dce.harvard.edu/dce-public/certificate/verify/4c12a86a-341c-4d0b-bba4-db6385551aef">Verified link</a>
+						</p>
+			
+						<p class="p-cs50">
+							I challenged myself to express art in code. It was <span>HTML and CSS</span> at first. Then I realize there's so much more to it. With my eagerness to learn it deeply I took different courses from variety of online school around the internet. 
+
+							I learn something new from time to time but the most rewarding was <span>Harvard's CS50 Intro to Computer Science</span>. </p>
+						</br></br>
+
+						<p class="p-cs50">
+							Check the link on the right. It has the Signature of the Senior lecturer on Computer Science of Harvard University, <a href="http://cs.harvard.edu/malan">David Malan</a> and Huntington D. Lambert, Dean of Harvard Extension School.</p>
+						<br/><br/>
+
+						<!--
+					
+							I've Learnt a few CMS, Languages, Frameworks, Extensions,  Command line, Git, Database and so many other stuff that I've never known before.<strong></strong><br/>
+							<strong>I'm having a lot of fun </strong>playing around the tools and techniques that I see around around the internet towards front end web development.<br/>
+
+						-->
+							</br>
+						
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="section3">
+		<div class="container3" id="timeline">
+			<div class="timeline">
+				<iframe src='http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AqxRHhGtSPoidGJjRzBzZzZoUkx6VWdSVTZLTklnNWc&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="section4">
+		<div class="Personal" id="personal">
+	    	<div class="slide">
+				<div class="intro">
+				<h1>My Better half</h1>
+				<p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.</p>
+				<img src="imgs/slider.png" alt="slider" />
+				</div>
+			</div>
+			<div class="slide">
+				<div class="intro">
+				<h1>My Better half</h1>
+				<p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.</p>
+				<img src="imgs/slider.png" alt="slider" />
+				</div>
+			</div>
+			<div class="slide">
+				<div class="intro">
+				<h1>My Better half</h1>
+				<p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.</p>
+				<img src="imgs/slider.png" alt="slider" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="section5">
+		<div class="Personal" id="contact">
+			<h1>My Better half</h1>
+			<p>Not only vertical scrolling but also horizontal scrolling. With fullPage.js you will be able to add horizontal sliders in the most simple way ever.</p>
+			<img src="imgs/slider.png" alt="slider" />
+		</div>
+	</div>
+
+</div>
+
 
 </div>
 </body>
